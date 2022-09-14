08b55dd9a1a0767bf44f793a09622b9c3fe97e9d
Dmytro Stetsenko <d.a.stetsenko@gmail.com>
2022-09-14 20:04:12 +0300

    add delete option

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,43 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React, { Component } from 'react';
+import nextId from "react-id-generator";
+
+export default class App extends Component  {
+  state = {
+    contacts: [{ id: nextId(), name: "Java" },
+    { id: nextId(), name: "Pyton" }],
+  name: ''
+  }
+  id = nextId();
+
+  deleteContact = (contactId) => {
+    this.setState((prev) =>( {
+      contacts: prev.contacts.filter(contact => contact.id !== contactId)
+    }))
+  }
+  
+  render() {
+    
+    return (
+      <>
+      <p>Phonebook</p>
+      <label>
+        <p>Name</p>
+        <input
+  type="text"
+  name="name"
+  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+  required
+        />
+        <button>Add contact</button>
+      </label>
+        <p>Contacts</p>
+        <ul>
+          {this.state.contacts.map(({id, name}) => {
+            return <li key={id}>{name}<button onClick={()=>{this.deleteContact(id)}}>Delete</button></li> 
+          })}
+        </ul>
+      </>
+    )
+  }
 };
