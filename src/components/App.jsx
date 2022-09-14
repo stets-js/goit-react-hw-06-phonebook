c28feec8a1c275ade4bb3a0e953a5c9eb2f5345b
Dmytro Stetsenko <d.a.stetsenko@gmail.com>
2022-09-14 22:47:33 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,43 +1,50 @@
 import React, { Component } from 'react';
-import nextId from "react-id-generator";
+import nextId from 'react-id-generator';
+import Form from './Form'
 
-export default class App extends Component  {
+export default class App extends Component {
   state = {
-    contacts: [{ id: nextId(), name: "Java" },
-    { id: nextId(), name: "Pyton" }],
-  name: ''
-  }
+    contacts: [],
+  };
+
   id = nextId();
 
-  deleteContact = (contactId) => {
-    this.setState((prev) =>( {
-      contacts: prev.contacts.filter(contact => contact.id !== contactId)
-    }))
+  deleteContact = contactId => {
+    this.setState(prev => ({
+      contacts: prev.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  submitHandler = ({ name, number }) => {
+    this.setState(prev => (
+      {
+       contacts: [...prev.contacts, {id: this.id, name: name, number: number}]
+     }))    
   }
-  
+
   render() {
-    
     return (
       <>
-      <p>Phonebook</p>
-      <label>
-        <p>Name</p>
-        <input
-  type="text"
-  name="name"
-  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-  required
-        />
-        <button>Add contact</button>
-      </label>
+        <p>Phonebook</p>
+        <Form onSubmit={this.submitHandler} />
         <p>Contacts</p>
         <ul>
-          {this.state.contacts.map(({id, name}) => {
-            return <li key={id}>{name}<button onClick={()=>{this.deleteContact(id)}}>Delete</button></li> 
+          {this.state.contacts.map(({ id, name, number }) => {
+            return (
+              <li key={id}>
+                {name}{number}
+                <button
+                  onClick={() => {
+                    this.deleteContact(id);
+                  }}
+                >
+                  Delete
+                </button>
+              </li>
+            );
           })}
         </ul>
       </>
-    )
+    );
   }
-};
+}
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+  state = {
+    name: '',
+    number: '',
+    };
+    
+formSubmit = e => {
+    e.preventDefault()
+    this.props.onSubmit(this.state)
+    this.resetForm()
+  }
+
+  inputChange = e => {
+    this.setState({[e.currentTarget.name]: e.currentTarget.value,});
+    };
+    
+    resetForm = () => {
+        this.setState({name: '',
+    number: '',})
+    }
+
+  render() {
+    return (
+      <form onSubmit={this.formSubmit}>
+        <label>
+          <p>Name</p>
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            value={this.state.name}
+            onChange={this.inputChange}
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+        <label>
+          <p>Number</p>
+          <input
+            type="tel"
+            name="number"
+            value={this.state.number}
+            onChange={this.inputChange}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </label>
+        <button>Add contact</button>
+      </form>
+    );
+  }
+}
+
+export default Form;
