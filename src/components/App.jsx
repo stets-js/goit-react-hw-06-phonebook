4fc533ff90a07c3d255b0977f1e72eafd7e5e467
Dmytro Stetsenko <d.a.stetsenko@gmail.com>
2022-09-18 19:33:45 +0300

    task1 localstorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,6 +16,21 @@ export default class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   deleteContact = contactId => {
     this.setState(prev => ({
       contacts: prev.contacts.filter(contact => contact.id !== contactId),
