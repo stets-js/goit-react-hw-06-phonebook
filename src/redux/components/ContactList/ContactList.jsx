99bb6df1f7f70f4ee3af8e18e565e11f00123558
Dmytro Stetsenko <d.a.stetsenko@gmail.com>
2022-10-01 22:04:48 +0300

    contactList

--- src/redux/components/ContactList/ContactList.jsx
+++ src/redux/components/ContactList/ContactList.jsx
@@ -1,62 +1,62 @@
 //import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
-import { removeContact } from 'redux/contactSlice';
-import css from './ContactList.module.css'
+import { removeContact } from '../../contactSlice';
+import css from './ContactList.module.css';
 
 const ContactList = () => {
   const dispatch = useDispatch();
-  const contacts = useSelector(state => state.contacts.value);
-  const filteredName = useSelector(state => state.filter.value)
+  const contacts = useSelector(state => state.contactsReduser.contacts);
+  //const filteredName = useSelector(state => state.contactsReduser.filter);
+  // if (filteredName === '') {
+    return (
+      <ul className={css.ul}>
+        {contacts.map(({ id, name, number }) => {
+          return (
+            <li className={css.li} key={id}>
+              <p>
+                {name}: {number}
+              </p>
+              <button
+                className={css.btn}
+                onClick={() => {
+                 dispatch(removeContact(id));
+                }}
+              >
+                Delete
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  // } else {
 
-    if (filteredName === '') {
-      return (
-        <ul className={css.ul}>
-          {contacts.map(({ id, name, number }) => {
-            return (
-              <li className={css.li} key={id}>
-                <p>
-                  {name}: {number}
-                </p>
-                <button
-                  className={css.btn}
-                  onClick={() => {
-                    dispatch(removeContact(id));
-                  }}
-                >
-                  Delete
-                </button>
-              </li>
-            );
-          })}
-        </ul>
-      );
-    } else {
-      
-      return (
-        <ul className={css.ul}>
-          {contacts.filter(contacts => contacts.name.toLowerCase().includes(filteredName.toLowerCase())).map(({ id, name, number }) => {
-            return (
-              <li className={css.li} key={id}>
-                <p>
-                  {name}: {number}
-                </p>
-                <button
-                  className={css.btn}
-                  onClick={() => {
-                     dispatch(removeContact(id));
-                  }}
-                >
-                  Delete
-                </button>
-              </li>
-            );
-          })}
-        </ul>
-      )
-      
-    }
-  }
-
+  // return (
+  //   <ul className={css.ul}>
+  //     {contacts
+  //       .find(contacts =>
+  //         contacts.name.toLowerCase().includes(filteredName.toLowerCase())
+  //       )
+  //       .map(({ id, name, number }) => {
+  //         return (
+  //           <li className={css.li} key={id}>
+  //             <p>
+  //               {name}: {number}
+  //             </p>
+  //             <button
+  //               className={css.btn}
+  //               onClick={() => {
+  //                 dispatch(removeContact(id));
+  //               }}
+  //             >
+  //               Delete
+  //             </button>
+  //           </li>
+  //         );
+  //       })}
+  //   </ul>
+  // );
+};
 
 // ContactList.propTypes = {
 //     contacts: PropTypes.arrayOf(PropTypes.shape({
@@ -65,6 +65,5 @@ const ContactList = () => {
 //         number: PropTypes.string.isRequired,
 //     })),
 //   }
-
 
 export default ContactList;
